2f8fe22cbac0ae79a42006d1816512cb78b419cd
Nabil Fannane <mzabi30@gmail.com>
2024-09-26 10:54:45 -0700

    Add more functionalities

--- dist/main.js
+++ dist/main.js
@@ -45,3 +45,18 @@ button.addEventListener('click', () => {
     }
 })
 
+
+var tablinks = document.getElementsByClassName("tab-links");
+    var tabcontents = document.getElementsByClassName("tab-contents");
+
+    function opentab(tabname){
+        for(tablink of tablinks){
+            tablink.classList.remove("active-link");
+        }
+        for(tabcontent of tabcontents){
+            tabcontent.classList.remove("active-tab");
+        }
+        event.currentTarget.classList.add("active-link");
+        document.getElementById(tabname).classList.add("active-tab");
+    }
+
